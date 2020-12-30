9eb8972de0e4736a555c8a39bdb5c8fb706fa8f1
minhquoc5599 <minhquoc5599@gmail.com>
2020-12-30 21:23:44 +0700

    add: layout and teacher view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('users/index', { title: 'Express' });
 });
+
+router.get('/list-teachers', function(req, res, next) {
+    res.render('teachers/list-teachers', { title: 'Express' });
+});
+
+router.get('/add-teacher', function(req, res, next) {
+    res.render('teachers/add-teacher', { title: 'Express' });
+});
+
+router.get('/edit-teacher', function(req, res, next) {
+    res.render('teachers/edit-teacher', { title: 'Express' });
+});
+
+
 
 module.exports = router;
