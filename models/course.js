c3f97259ff692c5f62fae0da3aa13d48dee05fd5
huskarit99 <nguyenthaihocxm1999@gmail.com>
2021-01-05 15:17:03 +0700

    add: model course

--- models/course.js
+++ models/course.js
@@ -3,9 +3,14 @@ const Schema = mongoose.Schema;
 
 var CourseSchema = new Schema({
     name: { type: String, require: true, max: 50 },
-    categories: [{
-        name: { type: String, require: true, max: 50 }
-    }]
+    image: { type: String, require: true, max: 100 },
+    description: { type: String, require: true, max: 200 },
+    detail: { type: String, require: true, max: 10000 },
+    price: { type: Number, require: true },
+    discount: { type: Number, require: true },
+    ownerId: { type: String, require: true, max: 50 },
+    categoryRootId: { type: Schema.Types.ObjectId, ref: 'Category' },
+    categoryChildCId: { type: String, require: true, max: 50 }
 });
 
 module.exports = mongoose.model('Course', CoureseSchema);
